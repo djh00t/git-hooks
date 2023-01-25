665f7e359bacd3c57633cf8c4ecc7fbf6d982a90
David Hooton <djh00t@users.noreply.github.com>
2023-01-25 16:52:32 +1100

    formatting
    Signed-off-by: David Hooton <djh00t@users.noreply.github.com>

--- includes/encrypt-age.sh
+++ includes/encrypt-age.sh
@@ -57,7 +57,7 @@ function do_get_files_encrypt() {
   # Get candidate files to encrypt
   export FILES_CAND=($(find $SEARCH_DIR \( -name "*.yml" -o -name "*.yaml" \) -exec grep -l -E '^(data:|stringData:)$' {} \;))
     echo -e "        There are ${#FILES_CAND} candidate files"
-    echo
+
   # Remove ignored files from the candidate files and store in $FILES_ENCRYPT
   for IGNORE_FILE in "${FILES_IGNORE[@]}"; do
     for CAND_FILE in "${FILES_CAND[@]}"; do
