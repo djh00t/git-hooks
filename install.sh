9888c8c053d22b9f6a01315cb0a9d5fe6b21d2a7
David Hooton <david@hooton.org>
2023-10-09 15:19:50 +1100

    Tidied up installer

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@
 ### Install commit hooks for git - run this script from the root of the repo
 ###
 
-# Make sure we're in the root of a git repo
-if ! git rev-parse --git-dir >/dev/null 2>&1; then
-  echo "Error: this script must be run from the root of a git repository"
+# Find root of git repo:
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# If we're not in a git repo, exit
+if [[ ! -d $REPO_ROOT ]]; then
+  echo "Error: Not in a git repo"
   exit 1
 fi
+
+# Move to the root of the repo
+cd $REPO_ROOT
 
 # Create the hooks directory if it doesn't exist
 echo "Making sure that the hooks directory exists..."
@@ -17,37 +23,52 @@ if [[ ! -d .git/hooks ]]; then
 fi
 
 # Install the pre-commit pip package if it isn't already installed
-echo "Installing the pre-commit pip package..."
-echo
-if [[ ! -d .git/hooks ]]; then
+PC_INSTALLED=$(which pre-commit)
+if [[ -z $PC_INSTALLED ]]; then
+  echo "pre-commit not installed, installing..."
+  echo
   pip install pre-commit
+else
+  echo "pre-commit already installed"
 fi
 
-# Install precommit into this repo if it isn't already installed
-echo "Installing pre-commit into this repo..."
-echo
-if [[ ! -d .git/hooks ]]; then
+# If .git/hooks/pre-commit doesn't exist, install pre-commit
+PC_REPO_INSTALLED=$(grep -c "pre-commit" .git/hooks/pre-commit)
+if [[ $PC_REPO_INSTALLED -eq 0 ]]; then
+  echo "pre-commit not installed in repo, installing..."
+  echo
   pre-commit install
+else
+  echo "pre-commit already installed in repo"
 fi
 
 # Make sure that an includes directory exists in the repo root
 echo "Making sure that the includes directory exists..."
 echo
+cd $REPO_ROOT
 if [[ ! -d includes ]]; then
   mkdir includes
 fi
 
-# Add git-hooks as a submodule in the repo includes directory
-cd includes
-echo "Adding git-hooks as a submodule..."
-git submodule add git@github.com:djh00t/git-hooks.git
-cd git-hooks
-git add .gitmodules git-hooks
-git commit -m "added git-hooks submodule"
+# Add git-hooks as a submodule in the repo includes directory if it isn't
+# already there
+echo "Making sure that the git_hooks submodule exists..."
+echo
+cd $REPO_ROOT/includes
+if [[ ! -d git_hooks ]]; then
+  git submodule add git@github.com:djh00t/git-hooks.git
+  cd git-hooks
+  git add .gitmodules git-hooks
+  git commit -m "added git-hooks submodule"
+  git push origin master
+else
+  echo "git_hooks submodule already exists"
+fi
 
 # Symlink the pre-commit hook
 echo "Symlinking the pre-commit hook..."
 echo
+cd $REPO_ROOT
 if [[ ! -f .git/hooks/pre-commit ]]; then
   ln -s includes/git_hooks/pre-commit .git/hooks/pre-commit
 else
@@ -58,6 +79,7 @@ fi
 # Symlink the post-commit hook
 echo "Symlinking the post-commit hook..."
 echo
+cd $REPO_ROOT
 if [[ ! -f .git/hooks/post-commit ]]; then
   ln -s includes/git_hooks/post-commit .git/hooks/post-commit
 else
